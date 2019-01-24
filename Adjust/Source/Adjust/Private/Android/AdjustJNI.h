6cd9a2e89ee2931a18edf0bbf90e451417df17ff
uerceg <ugi@adjust.com>
2019-01-24 11:07:30 +0100

    2019 year update in comments

--- Adjust/Source/Adjust/Private/Android/AdjustJNI.h
+++ Adjust/Source/Adjust/Private/Android/AdjustJNI.h
@@ -3,7 +3,7 @@
 //  Adjust SDK
 //
 //  Created by Uglješa Erceg (@uerceg) on 4th October 2018.
-//  Copyright © 2018 Adjust GmbH. All rights reserved.
+//  Copyright © 2018-2019 Adjust GmbH. All rights reserved.
 //
 
 #pragma once
